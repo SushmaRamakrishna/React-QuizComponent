d90e67b34497743776c65fecfebd38e64ce23e1c
SushmaRamakrishna <urpalshu@gmail.com>
2021-04-04 15:57:43 -0500

    commit4

--- src/Quiz.js
+++ src/Quiz.js
@@ -6,22 +6,27 @@ let quizData = require('./quiz_data.json')
 
 
 class Quiz extends Component {
-    
     constructor(props){
         super(props);
         this.state = {quiz_position: 1};
     }
+    showNextQuestion(){
+       this.setState((state) => {
+           return {quiz_position: state.quiz_position + 1}
+       })
+    }
     render(){
-        const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length
-            if(isQuizEnd)          
+        const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
+        return (<div>
             {
-               return  <div><QuizEnd /></div>
-            }
-            else
-            {
-                return  <div><QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} /></div>
+               isQuizEnd?   
+               <QuizEnd />:
+               <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} 
+                showNextQuestionHandler={this.showNextQuestion.bind(this)}
+               />
             }  
-        }         
+         </div>)
+    }     
 }
 
 export default Quiz
